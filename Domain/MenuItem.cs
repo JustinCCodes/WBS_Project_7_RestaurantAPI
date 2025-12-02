9d13438d7b43c3d2790fce0c1875a683123bca16
SmazeCode <209604287+SmazeCode@users.noreply.github.com>
2025-12-02 11:39:04 +0100

    - Added models - Setup Database

--- Domain/MenuItem.cs
+++ Domain/MenuItem.cs
@@ -2,5 +2,9 @@ namespace Restaurant.Api.Domain;
 
 public class MenuItem
 {
-    // TBD
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public required string Category { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -2,9 +2,14 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+await DbSeeder.SeedAsync(app);
 
 if (app.Environment.IsDevelopment())
 {
